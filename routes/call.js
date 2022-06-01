795b7c1c0cbdf267d613f53c8849a7d37f492647
Dennis <DennisLoska@users.noreply.github.com>
2022-06-01 18:52:55 +0200

    🚧 wip: test suite

--- routes/call.js
+++ routes/call.js
@@ -1,35 +1,57 @@
 const { Router } = require('express');
 const util = require('util');
 const fs = require('fs');
-
 const logger = require('../utils/logger');
 
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
-
 const router = Router();
 
-router.get(
-    '/call', async (req, res) => {
-        try {
-            const { id } = req.query;
-            console.log(id);
-            const buffer = await readFile(`${process.env.PWD}/data/calls.json`);
-            const calls = JSON.parse(buffer.toString('utf8'));
-            const call = calls.find((c) => c.id === id);
-            res.status(200).json(call);
-        } catch (error) {
-            logger.error(error);
-            res.status(500).json({ code: 500, error: 'Failed to load JSON file' });
-        }
-    }
-);
+// GET endpoint to fetch single call
+router.get('/call', async (req, res) => {
+    try {
+        // extract query parameters
+        const { id } = req.query;
 
-// GET endpoint to load calls from JSON file
+        // parameter validation
+        if (Number.isNaN(id)) {
+            return res.status(400).json({
+                code: 400,
+                error: 'Bad formatting of request parameters'
+            });
+        }
+
+        // get call data (think of an actual DB query here)
+        const buffer = await readFile(`${process.env.PWD}/data/calls.json`);
+        const calls = JSON.parse(buffer.toString('utf8'));
+
+        // general validation of operation
+        if (Array.isArray(calls)) {
+            const call = calls.find((c) => c.id.toString() === id);
+            if (call) {
+                return res.status(200).json(call);
+            }
+
+            // no call found
+            return res.status(404).json({ code: 404, error: 'Call not found' });
+        }
+        return res.status(500).json(
+            { code: 500, error: 'Internal server error' }
+        );
+    } catch (error) {
+        logger.error(error);
+        return res.status(500).json(
+            { code: 500, error: 'Failed to load JSON file' }
+        );
+    }
+});
+
+// GET endpoint to load all calls from JSON file
 router.get('/calls', async (req, res) => {
     try {
         const buffer = await readFile(`${process.env.PWD}/data/calls.json`);
         const json = JSON.parse(buffer.toString('utf8'));
+        // TODO: error handling
         res.status(200).json(json);
     } catch (error) {
         logger.error(error);
@@ -43,7 +65,7 @@ router.post('/calls', async (req, res) => {
         const data = JSON.stringify(req.body);
         await writeFile(`${process.env.PWD}/data/calls.json`, data, 'utf8');
         res.status(200).json(data);
-        // TODO#. do not overwrite whole file
+        // TODO: do not overwrite whole file
     } catch (error) {
         logger.error(error);
         res.status(500).json({ code: 500, error: 'Failed to write JSON file' });
--- routes/call.test.js
+++ routes/call.test.js
@@ -10,10 +10,63 @@ const api = '/api/v1';
 const app = express();
 app.use(`${api}/`, callRouter);
 
-describe('Get call', () => {
-    it('should return call by id', async () => {
+describe('GET call', () => {
+    it('should return a single call by id', async () => {
         const id = 2;
         const res = await request(app).get(`${api}/call?id=${id}`);
-        expect(res.body).toEqual({ oki: 'doki' });
+        expect(res.body).toEqual({
+            id: 2,
+            user: {
+                sms_number: '+491764777',
+                phone_number: '+49035356547',
+                name: 'Max Mustermann'
+            },
+            is_complete: true,
+            voicemails: [
+                {
+                    transcription: 'Ich möchte zu Dr. Müller'
+                },
+                {
+                    transcription: 'Heute oder Morgen'
+                }
+            ],
+            timestamp: '2021-04-22T10:29:19.891Z',
+            type: 'appointment'
+        });
     });
+    // TODO: add test cases
+});
+
+describe('GET calls', () => {
+    it('should return all existing calls', async () => {
+        const res = await request(app).get(`${api}/calls`);
+        expect(res.body).toEqual([
+            {
+                id: 1,
+                is_complete: false,
+                timestamp: '2021-04-23T12:29:19.891Z',
+                type: 'prescription',
+                user: { name: 'Thomas Meyer', phone_number: '+4903078544', sms_number: '+4917647914' },
+                voicemails: [{ transcription: ' Rezept' }, { transcription: 'Ibuprofen' }]
+            }, {
+                id: 2,
+                is_complete: true,
+                timestamp: '2021-04-22T10:29:19.891Z',
+                type: 'appointment',
+                user: { name: 'Max Mustermann', phone_number: '+49035356547', sms_number: '+491764777' },
+                voicemails: [{ transcription: 'Ich möchte zu Dr. Müller' }, { transcription: 'Heute oder Morgen' }]
+            },
+            {
+                id: 3,
+                is_complete: false,
+                timestamp: '2021-04-25T14:29:19.891Z',
+                type: 'emergency',
+                user: { name: 'Thomas Meyer', phone_number: '+490303457754', sms_number: '+491768964' },
+                voicemails: [
+                    { transcription: 'Ich habe starke Schmerzen' },
+                    { transcription: 'Ich habe mir das bein gebrochen aua' }]
+            }
+        ]);
+    });
+    // TODO: add test cases
 });
